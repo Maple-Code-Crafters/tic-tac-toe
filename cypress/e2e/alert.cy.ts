0d85d739423755cffbd9b11ac595bca149c10e07
DercilioFontes <dercilioafontes@gmail.com>
2024-03-27 22:22:49 -0400

    Formatted cypress files

--- cypress/e2e/alert.cy.ts
+++ cypress/e2e/alert.cy.ts
@@ -1,29 +1,27 @@
 /// <reference types="cypress" />
 
-describe("Player Name Alert Tests", () => {
-  it("When player1 name is not set then the alert shows the player1 message", () => {
-    cy.viewport("iphone-x");
-    cy.visit("/");
-    cy.get("ion-button").click();
-    cy.get(".alert-message").eq(0).contains("Please, enter player 1 name.");
+describe('Player Name Alert Tests', () => {
+  it('When player1 name is not set then the alert shows the player1 message', () => {
+    cy.viewport('iphone-x');
+    cy.visit('/');
+    cy.get('ion-button').click();
+    cy.get('.alert-message').eq(0).contains('Please, enter player 1 name.');
   });
 
-  it("When player1 name is set but player2 name is not set then the alert shows the player2 message", () => {
-    cy.viewport("iphone-x");
-    cy.visit("/");
-    cy.get("ion-input").eq(0).type("Player 1");
-    cy.get("ion-button").click();
-    cy.get(".alert-message").eq(0).contains("Please, enter player 2 name.");
+  it('When player1 name is set but player2 name is not set then the alert shows the player2 message', () => {
+    cy.viewport('iphone-x');
+    cy.visit('/');
+    cy.get('ion-input').eq(0).type('Player 1');
+    cy.get('ion-button').click();
+    cy.get('.alert-message').eq(0).contains('Please, enter player 2 name.');
   });
 
-
-  it("When player names are set then there is no alert", () => {
-    cy.viewport("iphone-x");
-    cy.visit("/");
-    cy.get("ion-input").eq(0).type("Player 1");
-    cy.get("ion-input").eq(1).type("Player 2");
-    cy.get("ion-button").click();
-    cy.get(".alert-message").should("have.length",0);
+  it('When player names are set then there is no alert', () => {
+    cy.viewport('iphone-x');
+    cy.visit('/');
+    cy.get('ion-input').eq(0).type('Player 1');
+    cy.get('ion-input').eq(1).type('Player 2');
+    cy.get('ion-button').click();
+    cy.get('.alert-message').should('have.length', 0);
   });
-
 });
--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
-    cy.contains("ion-content", "New Game");
-    cy.get("ion-card").should("have.length",1);
-    cy.get("ion-button").should("have.length",1);
-    cy.get("ion-input").should("have.length",2);
+describe('My First Test', () => {
+  it('Visits the app root url', () => {
+    cy.visit('/');
+    cy.contains('ion-content', 'New Game');
+    cy.get('ion-card').should('have.length', 1);
+    cy.get('ion-button').should('have.length', 1);
+    cy.get('ion-input').should('have.length', 2);
   });
 });
