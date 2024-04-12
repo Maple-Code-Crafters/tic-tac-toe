1c7e840f24d4d3783927fbedfa8da051c87342e1
DercilioFontes <dercilioafontes@gmail.com>
2024-04-11 21:36:34 -0400

    Added check for PROD to avoid sending development errors

--- src/sentry.ts
+++ src/sentry.ts
@@ -3,16 +3,18 @@ import * as SentrySibling from '@sentry/react';
 
 import { APP_NAME, APP_VERSION } from './constants';
 
-Sentry.init(
-  {
-    dsn: import.meta.env.VITE_SENTRY_DNS,
-    release: `${APP_NAME}@${APP_VERSION}`,
-    dist: '1',
-    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-    // We recommend adjusting this value in production.
-    tracesSampleRate: 1.0,
-    integrations: SentrySibling.defaultIntegrations,
-  },
-  // Forward the init method to the sibling Framework.
-  SentrySibling.init,
-);
+if (import.meta.env.PROD) {
+  Sentry.init(
+    {
+      dsn: import.meta.env.VITE_SENTRY_DNS,
+      release: `${APP_NAME}@${APP_VERSION}`,
+      dist: '1',
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      tracesSampleRate: 1.0,
+      integrations: SentrySibling.defaultIntegrations,
+    },
+    // Forward the init method to the sibling Framework.
+    SentrySibling.init,
+  );
+}
