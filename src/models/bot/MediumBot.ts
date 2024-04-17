f00f88772a2dac8ba54beeaa28ee097be9d7ba37
Rodolfo Vasconcelos <ravasconcelos@gmail.com>
2024-04-17 19:48:42 -0400

    Implement medium level

--- src/models/bot/MediumBot.ts
+++ src/models/bot/MediumBot.ts
@@ -1,12 +1,22 @@
-import type { Game, Index } from '../Game';
+import { type Game, type Index, Level } from '../Game';
 import type { Bot } from './Bot';
+import { BotBuilder } from './BotBuilder';
 
 export class MediumBot implements Bot {
   public chooseMove(game: Game): Index | undefined {
+    // const flippedCoin = Math.floor(Math.random() * 2);
+    // console.log('flippedCoin', flippedCoin);
+    // if (flippedCoin === 0) {
+    //   return BotBuilder.build(Level.Easy).chooseMove(game);
+    // } else {
+    //   return BotBuilder.build(Level.Hard).chooseMove(game);
+    // }
+
     const availableCells = game.getAvailableCells();
-    if (game.finished() || game.hasWin() || !availableCells.length) {
-      return;
+    if ((availableCells.length / 2) % 2 === 0) {
+      return BotBuilder.build(Level.Easy).chooseMove(game);
+    } else {
+      return BotBuilder.build(Level.Hard).chooseMove(game);
     }
-    return availableCells[0];
   }
 }
