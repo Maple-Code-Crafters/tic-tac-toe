35f2882f121e57141d7da009cc5f5532abcdb35e
DercilioFontes <dercilioafontes@gmail.com>
2024-04-27 22:35:50 -0400

    Removed use of variables.css and updated browserslist

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { IonReactRouter } from '@ionic/react-router';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
+import '@ionic/react/css/palettes/dark.system.css';
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
@@ -26,8 +27,6 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
-/* Theme variables */
-import './theme/variables.css';
 import './App.css';
 
 import { useAppInit } from './hooks';
