ccb5ffa96c70312f9cc36418df08111a0efc94ea
DercilioFontes <dercilioafontes@gmail.com>
2024-05-13 21:28:48 -0400

    Added test for Play page form elements

--- src/pages/Play.test.tsx
+++ src/pages/Play.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '../test-utils';
+import { render, screen } from '../test-utils';
 import PlayPage from './Play';
 
 describe('PlayPage', () => {
@@ -7,5 +7,19 @@ describe('PlayPage', () => {
     expect(baseElement).toBeDefined();
   });
 
-  // TODO: add more tests
+  test('has proper texts and elements', () => {
+    render(<PlayPage />);
+    expect(screen.getByText('New Game')).toBeVisible();
+    expect(screen.getByText('Select Players')).toBeVisible();
+    expect(screen.getByText('One Player')).toBeVisible();
+    expect(screen.getByText('Two Players')).toBeVisible();
+    expect(screen.getByText('Easy')).toBeVisible();
+    expect(screen.getByText('Medium')).toBeVisible();
+    expect(screen.getByText('Hard')).toBeVisible();
+    expect(screen.getAllByText('O')).toHaveLength(2);
+    expect(screen.getAllByText('X')).toHaveLength(2);
+    expect(screen.getByPlaceholderText('Enter player 1 name')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Enter player 2 name')).toBeInTheDocument();
+    expect(screen.getByText('Start')).toBeVisible();
+  });
 });
