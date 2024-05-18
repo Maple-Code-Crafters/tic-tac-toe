0b63dd12024a44271364c3ff5d39905c16baae47
DercilioFontes <dercilioafontes@gmail.com>
2024-05-17 22:10:31 -0400

    Added Two players and player 1 wins

--- src/pages/Play.test.tsx
+++ src/pages/Play.test.tsx
@@ -6,6 +6,7 @@ import PlayPage from './Play';
 describe('PlayPage', () => {
   test('renders without crashing', () => {
     const { baseElement } = render(<PlayPage />);
+
     expect(baseElement).toBeDefined();
   });
 
@@ -23,19 +24,17 @@ describe('PlayPage', () => {
     expect(screen.getAllByText('X')).toHaveLength(2);
     expect(screen.getByDisplayValue('Player 1')).toBeVisible();
     expect(screen.getByDisplayValue('CPU (Easy)')).toBeVisible();
-    const startButton = screen.getByText('Start');
-    expect(startButton).toBeVisible();
-    expect(startButton).toContainHTML('ion-button');
+    expect(screen.getByText('Start', { selector: 'ion-button' })).toBeVisible();
   });
 
   test('selecting two players removes CPU level options', async () => {
     const user = userEvent.setup();
     render(<PlayPage />);
     await safeAct();
-    expect(screen.getByDisplayValue('CPU (Easy)')).toBeVisible();
     const easy = screen.getByText('Easy');
     const medium = screen.getByText('Medium');
     const hard = screen.getByText('Hard');
+    screen.getByDisplayValue('CPU (Easy)');
     await user.click(screen.getByText('Two Players'));
     expect(easy).not.toBeVisible();
     expect(medium).not.toBeVisible();
@@ -55,9 +54,7 @@ describe('PlayPage', () => {
     for (const cell of cells) {
       expect(cell).toHaveTextContent('');
     }
-    const cancelButton = screen.getByText('Cancel');
-    expect(cancelButton).toBeVisible();
-    expect(cancelButton).toContainHTML('ion-button');
+    expect(screen.getByText('Cancel', { selector: 'ion-button' })).toBeVisible();
   });
 
   test('clicking cancel cancels a game', async () => {
@@ -68,5 +65,28 @@ describe('PlayPage', () => {
     expect(screen.getByText('Player turn')).toBeVisible();
     await user.click(screen.getByText('Cancel'));
     expect(screen.getByText('Select Players')).toBeVisible();
+  });
+
+  test('Two players game and player 1 wins the game', async () => {
+    const user1 = userEvent.setup();
+    const user2 = userEvent.setup();
+    const { container } = render(<PlayPage />);
+    await safeAct();
+    await user1.click(screen.getByText('Two Players'));
+    await user1.click(screen.getByText('Start'));
+    const cell1 = container.querySelector('.cell-1 > div');
+    await user1.click(cell1!);
+    const cell2 = container.querySelector('.cell-2 > div');
+    await user2.click(cell2!);
+    const cell4 = container.querySelector('.cell-5 > div');
+    await user1.click(cell4!);
+    const cell5 = container.querySelector('.cell-6 > div');
+    await user2.click(cell5!);
+    const cell9 = container.querySelector('.cell-9 > div');
+    await user1.click(cell9!);
+    expect(await screen.findByText('Player 1')).toBeVisible();
+    expect(await screen.findByText('Winner')).toBeVisible();
+    expect(screen.getByText('Play Again')).toBeVisible();
+    expect(screen.getByText('New Game', { selector: 'ion-button' })).toBeVisible();
   });
 });
